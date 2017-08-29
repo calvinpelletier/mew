ac3117b4e68b79fca2dfaa390a80b6c5533ef208
Calvin Pelletier <calvin.pelletier@gmail.com>
2017-08-29 10:05:58 -0700

    untested backend event summarizing

---
+++ server/event_analysis.py
@@ -0,0 +1,27 @@
+import event_storage
+import time
+
+
+def get_last_x_min_summary(db, uid, x_min):
+    start_time = (time.time() - x_min) * 1000 # sec to ms
+    events = event_storage.select(db, uid, start_time)
+
+    prev_ts = None
+    prev_hostname = None
+    summary = {}
+    for event in events:
+        hostname = event[0]
+        ts = event[1]
+        if prev_ts is not None:
+            if ts < prev_ts:
+                # something went horribly wrong in our database
+                # or someone removed ORDER BY from select statement
+                raise Exception('events not in order')
+            if hostname in summary:
+                summary[hostname] += (ts - prev_ts) / (1000. * 60.)
+            else:
+                summary[hostname] = (ts - prev_ts) / (1000. * 60.)
+        prev_ts = ts
+        prev_hostname = hostname
+
+    return summary
--- server/event_storage.py
+++ server/event_storage.py
@@ -1,5 +1,6 @@
 import authentication
 
+
 def insert(db, web_event):
     # currently the only case we handle:
     uid = authentication.token_to_uid(db, web_event.token)
@@ -10,3 +11,16 @@ def insert(db, web_event):
     c.execute('INSERT INTO events VALUES (?, ?, ?)', (uid, web_event.hostname, web_event.time))
     db.commit()
     c.close()
+
+
+# start_time/end_time in unixtime (ms)
+def select(db, uid, start_time, end_time=None):
+    c = db.cursor()
+    if end_time is None:
+        c.execute('SELECT hostname, ts FROM events WHERE uid = ? AND ts > ? ORDER BY ts ASC', (uid, start_time))
+    else:
+        c.execute('SELECT hostname, ts FROM events WHERE uid = ? AND ts > ? AND ts < ? ORDER BY ts ASC', (uid, start_time, end_time))
+    events = c.fetchall()
+    c.close()
+
+    return events
