101476bdc3f9bdb35cff014e040b569ad715ff1d
Wilson Chaney <wilsongchaney@gmail.com>
2017-10-09 15:17:55 -0500

    actually fix file logging

--- server/core/log.py
+++ server/core/log.py
@@ -1,9 +1,19 @@
+import logging
 import logging.config
+import logging.handlers
 from os import path, mkdir
 
 
 def init_loggers(mew_path):
-    file_output_path = path.join(mew_path, "server/.log")
-    if not path.exists(file_output_path):
-        mkdir(file_output_path)
+    logfile_dir = path.join(mew_path, "server/.log")
+    logfile_path = path.join(logfile_dir, "out")
+    if not path.exists(logfile_dir):
+        mkdir(logfile_dir)
     logging.config.fileConfig(path.join(mew_path, "server/config/logging.conf"))
+
+    # Create file handler for main logs
+    file_handler = logging.handlers.RotatingFileHandler(logfile_path, maxBytes=5e7)
+    formatter_w_date = \
+        logging.Formatter("%(asctime)s [%(name)s - %(levelname)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+    file_handler.setFormatter(formatter_w_date)
+    logging.getLogger("main").addHandler(file_handler)
