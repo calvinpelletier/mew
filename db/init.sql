55cdc631cdf7f08179fb5af7fdd4737fe3698107
Calvin Pelletier <calvin.pelletier@gmail.com>
2017-10-14 17:46:13 -0500

    changed db schema for quotas and list of unproductive websites

--- db/init.sql
+++ db/init.sql
@@ -27,3 +27,15 @@ CREATE TABLE IF NOT EXISTS daily_summary_cache (
     ts BIGINT, /* ts of day start */
     json VARCHAR(2147483647)
 );
+
+CREATE TABLE IF NOT EXISTS unprod_sites (
+    uid INTEGER PRIMARY KEY,
+    json VARCHAR(2147483647)
+);
+
+CREATE TABLE IF NOT EXISTS quotas (
+    uid INTEGER PRIMARY KEY,
+    quota INTEGER, /* minutes */
+    streak INTEGER, /* days */
+    streak_last_updated BIGINT /* UTC ts of day start */
+);
