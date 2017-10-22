4e2f8cf2d891b7bfb83d441c19d939b5739e59ff
Calvin Pelletier <calvin.pelletier@gmail.com>
2017-10-22 16:45:48 -0500

    fixed settings modal so that you can't interact with the background when its open

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -59,14 +59,28 @@ window.onload = function() {
 	setQuotaPercent(65);
 
 	$('#settings-icon').on('click', function(e) {
-		console.log("Clicked settings.");
-		$('#body-container').attr('class', 'blur');
-		$('#settings-dialog').show();
-
-		// Temporary - just to close the dialog
-		$('#settings-dialog').on('click', function(e) {
-			$('#body-container').removeClass('blur');
-			$('#settings-dialog').hide();
-		});
+		$('#settings-container').removeClass('hidden');
+		$('#body-container').addClass('blur');
+		$('#body-container').addClass('no-scrolling');
 	});
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+		if (event.target == document.getElementById('settings-container')) {
+			$('#settings-container').addClass('hidden');
+			$('#body-container').removeClass('blur');
+			$('#body-container').removeClass('no-scrolling');
+		}
+	}
+
+	// $('#settings-cancel').on('click', function(e) {
+	// 	$('#body-container').removeClass('blur');
+	// 	$('#settings-dialog').hide();
+	// });
+	//
+	// $('#settings-save').on('click', function(e) {
+	// 	// TODO: post
+	// 	$('#body-container').removeClass('blur');
+	// 	$('#settings-dialog').hide();
+	// });
 };
