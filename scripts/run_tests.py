e6439865e11b44e0c6612d9f71f3d130ab92b4b6
Wilson Chaney <wilsongchaney@gmail.com>
2017-10-24 09:26:44 -0500

    re-arrange unit tests

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -4,6 +4,4 @@ from os import environ, path, chdir
 
 mew_server_root = path.join(environ["MEW_PATH"], "server")
 chdir(mew_server_root)
-environ["PYTHONPATH"] = mew_server_root
-full_test_path = path.join(mew_server_root, "test/test_main.py")
-exit(subprocess.call("python %s" % full_test_path, shell=True, env=environ))
+exit(subprocess.call("python -m unittest test.test_main", shell=True, env=environ))
