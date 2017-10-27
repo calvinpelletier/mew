bc438247b03db476e3ed4262028157064c4c6438
Wilson Chaney <wilsongchaney@gmail.com>
2017-10-27 17:05:45 -0500

    fix changing site selection

--- server/static/js/line_graph.js
+++ server/static/js/line_graph.js
@@ -1,6 +1,9 @@
 var LG_FAIL_PLACEHOLDER = "Failed to load line graph data.";
 var LG_NO_DATA_PLACEHOLDER = "No data found for line graph.";
 
+// Highcharts object
+var lineGraph;
+
 function filterAndDrawLineGraph() {
     if (window.raw_line_graph_data) {
         let filteredData = filterData(window.raw_line_graph_data.data, getLineGraphMinutes());
@@ -37,6 +40,20 @@ function drawLineGraphFailure(message) {
 function drawLineGraph(timestamp_labels, data, divId) {
 	var N_VISIBLE_DOMAINS = 4;
 
+	visible = {};
+	if (lineGraph) {
+        for (var i = 0;i < lineGraph.series.length; i++) {
+            if (lineGraph.series[i].visible) {
+                visible[lineGraph.series[i].name] = true;
+            }
+        }
+	} else {
+	    var domainList = Object.keys(data);
+	    for (var i = 0;i < N_VISIBLE_DOMAINS; i++) {
+	        visible[domainList[i]] = true;
+	    }
+	}
+
 	var chartData = [];
 	var i = 0;
 	for (var domain in data) {
@@ -45,97 +62,100 @@ function drawLineGraph(timestamp_labels, data, divId) {
 		  return [ts.getTime(), data[domain][i]];
 		});
 
+		let _visible = (domain in visible) ? true : false;
+
 		chartData.push({
 			name: domain,
 			data: fullData,
-			visible: i < N_VISIBLE_DOMAINS
+			visible: _visible
 		});
 		i++;
 	}
 
 	var scrollTop = $(window).scrollTop();
 
-	Highcharts.chart(divId, {
-		title: {text: null},
-		yAxis: {
-			title: {
-				text: 'minutes',
-				style: {
-					textTransform: 'uppercase'
-				}
-			},
-			minorTickInterval: 'auto',
-			labels: {
-				style: {
-					fontSize: '12px'
-				}
-			}
-		},
-		legend: {
-			layout: 'vertical',
-			align: 'right',
-			verticalAlign: 'middle',
-			itemStyle: {
-				fontWeight: 'bold',
-				fontSize: '13px'
-			},
-			margin: 50
-		},
-		xAxis: {
-			type: 'datetime',
-	        labels: {
-	            format: '{value:%Y-%m-%d}',
-	            rotation: 45,
-	            align: 'left',
-				style: {
-					fontSize: '12px'
-				}
-	        },
-			gridLineWidth: 1,
-		},
-		tooltip: {
-			pointFormatter: function() {
-				return this.series.name + ': ' + formatTime(this.y);
-			},
-			borderWidth: 0,
-			backgroundColor: 'rgba(219,219,216,0.8)',
-			shadow: false
-		},
-		series: chartData,
-		responsive: {
-			rules: [{
-				condition: {
-					maxWidth: 500
-				},
-				chartOptions: {
-					legend: {
-						layout: 'horizontal',
-						align: 'center',
-						verticalAlign: 'bottom'
-					}
-				}
-			}]
-		},
-		credits: {
-	        enabled: false
-	    },
+    lineGraph = Highcharts.chart(divId, {
+        title: {text: null},
+        yAxis: {
+            title: {
+                text: 'minutes',
+                style: {
+                    textTransform: 'uppercase'
+                }
+            },
+            minorTickInterval: 'auto',
+            labels: {
+                style: {
+                    fontSize: '12px'
+                }
+            }
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            itemStyle: {
+                fontWeight: 'bold',
+                fontSize: '13px'
+            },
+            margin: 50
+        },
+        xAxis: {
+            type: 'datetime',
+            labels: {
+                format: '{value:%Y-%m-%d}',
+                rotation: 45,
+                align: 'left',
+                style: {
+                    fontSize: '12px'
+                }
+            },
+            gridLineWidth: 1,
+        },
+        tooltip: {
+            pointFormatter: function() {
+                return this.series.name + ': ' + formatTime(this.y);
+            },
+            borderWidth: 0,
+            backgroundColor: 'rgba(219,219,216,0.8)',
+            shadow: false
+        },
+        series: chartData,
+        responsive: {
+            rules: [{
+                condition: {
+                    maxWidth: 500
+                },
+                chartOptions: {
+                    legend: {
+                        layout: 'horizontal',
+                        align: 'center',
+                        verticalAlign: 'bottom'
+                    }
+                }
+            }]
+        },
+        credits: {
+            enabled: false
+        },
 
-		// theme stuff
-		colors: ['#7cb5ec', '#f7a35c', '#90ee7e', '#7798BF', '#aaeeee', '#ff0066',
-		'#eeaaee', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
-		chart: {
-			backgroundColor: null,
-			style: {
-				fontFamily: 'Sinkin-Sans200XLight, sans-serif'
-			}
-		},
-		plotOptions: {
-			candlestick: {
-				lineColor: '#404048'
-			}
-		},
-		background2: '#F0F0EA'
-	});
+        // theme stuff
+        colors: ['#7cb5ec', '#f7a35c', '#90ee7e', '#7798BF', '#aaeeee', '#ff0066',
+        '#eeaaee', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
+        chart: {
+            backgroundColor: null,
+            style: {
+                fontFamily: 'Sinkin-Sans200XLight, sans-serif'
+            }
+        },
+        plotOptions: {
+            candlestick: {
+                lineColor: '#404048'
+            }
+        },
+        background2: '#F0F0EA'
+    });
+
 
 	$(window).scrollTop(scrollTop);
 }
