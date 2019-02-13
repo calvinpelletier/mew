fc08d53914bd271fdd801f218f3b1f9950643a3d
Calvin Pelletier <calvin.pelletier@gmail.com>
2019-02-12 23:45:14 -0800

    small fix

--- server/core/calvin.py
+++ server/core/calvin.py
@@ -7,14 +7,9 @@ import event_analysis
 import concurrency
 
 concurrency.init()
-
-database_path = environ.get('MEW_DB_PATH')
-if not database_path:
-    exit(1)
-db = sqlite3.connect(database_path)
+db = sqlite3.connect('/var/www/mew/db/prod.db')
 
 data = event_analysis.get_daily_summary(db, 4, 'America/Los_Angeles')['data']
-
 for day in data:
     date = datetime.datetime.utcfromtimestamp(day['date']).date().isoformat()
     usage = int(day['summary']['reddit.com']) if 'reddit.com' in day['summary'] else 0
