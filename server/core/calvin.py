6154df53df10cf1c53bc0a8706dc994b65f4b4df
Calvin Pelletier <calvin.pelletier@gmail.com>
2019-02-13 00:03:46 -0800

    small change

--- server/core/calvin.py
+++ server/core/calvin.py
@@ -12,7 +12,8 @@ log.init_loggers('/var/www/mew/')
 db = sqlite3.connect('/var/www/mew/db/prod.db')
 
 data = event_analysis.get_daily_summary(db, 4, 'America/Los_Angeles')['data']
-for day in data:
-    date = datetime.datetime.utcfromtimestamp(day['date']).date().isoformat()
-    usage = int(day['summary']['reddit.com']) if 'reddit.com' in day['summary'] else 0
-    print '{},{}'.format(date, usage)
+with open('/tmp/calvin_reddit.csv', 'w') as f:
+    for day in data:
+        date = datetime.datetime.utcfromtimestamp(day['date']).date().isoformat()
+        usage = int(day['summary']['reddit.com']) if 'reddit.com' in day['summary'] else 0
+        f.write('{},{}\n'.format(date, usage))
